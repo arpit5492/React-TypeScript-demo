b330aad40f9e2b696970e7a06606fd5e2824a885
arpit5492 <arpitmallick99@gmail.com>
2024-04-08 01:41:11 -0700

    Added select element demo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Product from './components/Product';
 function App() {
   const [cnt, setCounter] = useState<number>(0);
   const [txt, changeTxt] = useState<string>("");
+  const [dropTxt, changeDropTxt] = useState<string>("");
 
   const handleClick = (e:React.MouseEvent<HTMLButtonElement>):void => {
     setCounter(cnt+1);
@@ -12,6 +13,10 @@ function App() {
 
   const inputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
     changeTxt(e.target.value);
+  }
+
+  const dropHandler = (e:React.ChangeEvent<HTMLSelectElement>) => {
+    changeDropTxt(e.target.value);
   }
   return (
     <div className="App">
@@ -22,6 +27,14 @@ function App() {
       <h3>{txt}</h3>
       <hr />
       <input type="text" onChange={inputChange}/>
+      <hr />
+      <h3>{dropTxt}</h3>
+      <select onChange={dropHandler}>
+        <option value="Apples">Apples</option>
+        <option value="Bananas">Bananas</option>
+        <option value="Oranges">Oranges</option>
+        <option value="Grapes">Grapes</option>
+      </select>
     </div>
   );
 }
