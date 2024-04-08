c8f24e5f367eabc490aaba620b3ee14ac5cd6c82
arpit5492 <arpitmallick99@gmail.com>
2024-04-08 01:34:48 -0700

    Added input element demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Product from './components/Product';
 
 function App() {
-  const [cnt, setCounter] = useState<number|string|null|boolean>("");
-  useEffect(() => {
-    setCounter(true);
-  }, []);
+  const [cnt, setCounter] = useState<number>(0);
+  const [txt, changeTxt] = useState<string>("");
+
+  const handleClick = (e:React.MouseEvent<HTMLButtonElement>):void => {
+    setCounter(cnt+1);
+  }
+
+  const inputChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+    changeTxt(e.target.value);
+  }
   return (
     <div className="App">
       <Product pCode = {1} pName = {"Apples"} qty = {10}/>
-      <h2>{JSON.stringify(cnt)}</h2>
+      <h2>{cnt}</h2>
+      <button onClick={handleClick}>Increment</button>
+      <hr />
+      <h3>{txt}</h3>
+      <hr />
+      <input type="text" onChange={inputChange}/>
     </div>
   );
 }
